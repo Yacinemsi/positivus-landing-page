1d605791ebea5b13389e4738615fb0a7a09ff991
Moussaoui Yacine <contact@yacinemoussaoui.com>
2024-11-22 17:20:00 +0100

    changement de padding dans la listcards

--- src/components/ListCards.js
+++ src/components/ListCards.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import CustomButton from "./CustomButton";
 
 const ListCards = () => {
   const cardsData = [
@@ -76,10 +77,33 @@ const ListCards = () => {
           businesses grow and succeed online. These services include:
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pb-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 pb-12">
         {cardsData.map((card, index) => (
           <Card key={index} {...card} />
         ))}
+      </div>
+      <div className="flex justify-between bg-gray-100 rounded-[28px] p-8 gap-4 mb-20">
+        <div className="flex flex-col justify-center items-center md:items-start gap-4 flex-1">
+          <h3 className="text-xl md:text-2xl font-medium">
+            Let’s make things happen
+          </h3>
+          <p className="text-sm md:text-base text-justify">
+            Contact us today to learn more about how our digital marketing
+            services can help your business grow and succeed online.
+          </p>
+          <CustomButton
+            classNameDiv="max-w-60 w-full dark:hover:border-b-black"
+            classNameButton="w-full bg-black text-white max-w-60 hover:text-black"
+            text="Get your proposal"
+          />
+        </div>
+        <div className="hidden md:flex flex-1 relative justify-center items-center">
+          <img
+            src="/assets/illustration-letsmake.svg"
+            alt="illustration"
+            className="absolute w-60"
+          />
+        </div>
       </div>
     </>
   );
